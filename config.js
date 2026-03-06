ef230bf153699c008ae2a1f1c3fb64226b56d2ea
DominicDev <s0l0estudi4r@gmail.com>
2026-03-06 17:10:54 +0100

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'PHILLIP PHILLIPS';
+export const nameSongs = [
+	"RAGING FIRE", /*1*/
+	"GONE, GONE, GONE", /*2*/
+	"HOME", /*3*/
+	"UNPACK YOUR HEART", /*4*/
+	"DANCING WITH YOUR SHADOWS", /*5*/
+	"BEFORE I LOVED YOU", /*6*/
+	"LOVE LIKE THAT", /*7*/
+	"OVER", /*8*/
+	"CALM BEFORE THE STORM", /*9*/
+	"DRIFT BACK TOGETHER", /*10*/
+	"LOVE COME BACK TO ME", /*11*/
+	"LONG TIME", /*12*/
+	"STRANGERS", /*13*/
+	"SO WILL I", /*14*/
+	"PART OF MY PLAN", /*15*/
+	"MY NAME", /*16*/
+	"I DARE YOU", /*17*/
+	"SAND CASTLES", /*18*/
+	"DON'T TELL ME", /*19*/
+	"TRIGGER", /*20*/
+]
